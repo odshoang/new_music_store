821c004772b730bb0f56ceb7314594ea33325d96
odshoang <33654686+odshoang@users.noreply.github.com>
2017-11-14 17:13:03 +0700

    create new project

---
+++ temp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(temp.Startup))]
+namespace temp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
